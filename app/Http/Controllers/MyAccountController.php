d8324317c7651de2666ee7fcecf57e80379d2ca2
LunarRoberstein <bulansyfa3@gmail.com>
2026-06-23 14:21:12 +0800

    fix: perbaiki getId menjadi id di MyAccountController

--- app/Http/Controllers/MyAccountController.php
+++ app/Http/Controllers/MyAccountController.php
@@ -10,21 +10,10 @@ class MyAccountController extends Controller
 {
     public function orders()
     {
-        /** @var \App\Models\User $user */
-        $user = Auth::user();
-
         $viewData = [];
-        $viewData["title"] = "My Orders - Bulan's Store";
-        $viewData["subtitle"] = "My Orders";
-
-        $viewData["orders"] = Order::where(
-            'user_id',
-            $user->getId()
-        )->get();
-
-        return view('myaccount.orders')
-            ->with("viewData", $viewData);
-
-        dd(view()->exists('myaccount.orders'));
+        $viewData["title"] = "My Orders - Online Store";
+        $viewData["subtitle"] =  "My Orders";
+        $viewData["orders"] = Order::with(['items.product'])->where('user_id', Auth::user()->id)->get();
+        return view('myaccount.orders')->with("viewData", $viewData);
     }
 }

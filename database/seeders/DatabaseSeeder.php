7a391dc99e525b42b064de8f7a8fac9f43e92693
LunarRoberstein <bulansyfa3@gmail.com>
2026-06-23 16:40:21 +0800

    fix: sesuaikan kolom product di seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,33 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    use WithoutModelEvents;
+
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Product::create([
+            'description' => 'Product 1',
+            'price' => 10.00,
+            'image' => 'game.png',
+        ]);
+
+        Product::create([
+            'description' => 'Product 2',
+            'price' => 20.00,
+            'image' => 'safe.png',
+        ]);
     }
 }
